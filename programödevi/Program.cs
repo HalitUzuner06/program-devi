80148179d4d41a30d6905039717189e0bc32e1f3
uzune <uzune@HALIT>
2024-01-11 09:55:51 +0300

    Proje dosyası ekle.

---
+++ programödevi/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Programodev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       
+            {
+                Console.WriteLine("Enter the starting number : ");
+                byte baslangıc = byte.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the ending number : ");
+                int bitis = int.Parse(Console.ReadLine());
+                Console.WriteLine("How many numbers do you want? : ");
+                int numbers = int.Parse(Console.ReadLine());
+
+                Random rnd = new Random();
+                int[] sayi = new int[numbers];
+                int number;
+                for (int i = 0; i < sayi.Length; i++)
+                {
+                    number = rnd.Next(baslangıc, bitis);
+                    if (i == 0)
+                    {
+                        sayi[i] = number;
+                    }
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (sayi[j] == number)
+                        {
+                            i--;
+                            break;
+                        }
+                        else
+                        {
+                            sayi[i] = number;
+                        }
+                    }
+                }
+                Array.Sort(sayi);
+                for (int i = 0; i < sayi.Length; i++)
+                {
+                    Console.WriteLine(sayi[i]);
+                }
+            Console.ReadKey();
+     
+        }
+
+    }
+}
